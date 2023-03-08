eeca39f62b8ffbaf428aac8955236b3ef71a5574
imuslih <imuslih.muhamad@gmail.com>
2023-03-08 07:41:24 +0700

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index (){
+
+        $data['user'] = 'Imam Muslih';
+
+        // ORM
+        $data['modelUser'] = User::all();
+
+        return view ('index', $data, $modelUser);
+    }
+}
